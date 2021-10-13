6f251ab966ed22abd77fb3f8d4bef434d3d8998b
Naveen AutomationLabs <naveenanimation20@gmail.com>
2021-10-13 08:34:46 +0530

    added cart page

---
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class CartPage {
+	
+	public CartPage() {
+		System.out.println("cart page...");
+	}
+	
+	public void addToCart() {
+		System.out.println("add to cart");
+	}
+
+}
