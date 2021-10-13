697695e203321086f0b16f7e3ec997be91339c80
Naveen AutomationLabs <naveenanimation20@gmail.com>
2021-10-13 08:46:11 +0530

    updated Cart Page as per review comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,6 +1,10 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
+	
+	private By cartButton = By.id("cart");
 	
 	public CartPage() {
 		System.out.println("cart page...");
@@ -8,6 +12,7 @@ public class CartPage {
 	
 	public void addToCart() {
 		System.out.println("add to cart");
+		System.out.println("add feature is done....");
 	}
 
 }
