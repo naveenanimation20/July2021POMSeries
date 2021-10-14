fb76e84637966721dcc2c940e965d4d8fbc6fc3b
Naveen AutomationLabs <naveenanimation20@gmail.com>
2021-10-14 08:50:17 +0530

    product naveen

--- src/main/java/com/qa/opencart/pages/ProductDemo.java
+++ src/main/java/com/qa/opencart/pages/ProductDemo.java
@@ -5,7 +5,9 @@ public class ProductDemo {
 	public static void main(String[] args) {
 
 		System.out.println("demo 1");
-		
+		System.out.println("demo 3");
+		System.out.println("demo 4");
+
 		
 	}
 
